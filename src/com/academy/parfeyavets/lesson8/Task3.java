1fae36cca17a9e4ba6d39a379d803e18e839afc7
Valery Borys <valeryborys@gmail.com>
2023-06-09 20:59:10 +0300

    check7-8

--- src/com/academy/parfeyavets/lesson8/Task3.java
+++ src/com/academy/parfeyavets/lesson8/Task3.java
@@ -2,7 +2,7 @@ package com.academy.parfeyavets.lesson8;
 
 public class Task3 {
     public static void main(String[] args) {
-        Book BOOK = new Book();// не доделал
+        Book BOOK = new Book();// не доделал //доделывай :)
 
 
 
--- src/com/academy/parfeyavets/lesson9/Task1.java
+++ src/com/academy/parfeyavets/lesson9/Task1.java
@@ -2,7 +2,7 @@ package com.academy.parfeyavets.lesson9;
 
 public class Task1 {
     public static void main(String[] args) {
-        GenericArray genericArray = new GenericArray<>(new Integer[]{2,5,7});
+        GenericArray genericArray = new GenericArray<>(new Integer[]{2,5,7}); //сырой тип. укажи параметризацию GenericArray<Integer>
         genericArray.printElement(1);
     }
 }
